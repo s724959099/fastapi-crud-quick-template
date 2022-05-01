a5f7d6644e29cda615844b20c430a3eaed2d4e3c
Max <s724959099@yahoo.com.tw>
2022-05-01 17:16:51 +0800

    fix: add crud

--- app/api/api_crud.py
+++ app/api/api_crud.py
@@ -5,6 +5,35 @@ from api.deps import Pagination, get_pagination_schema
 import typing
 
 
+class RouterAbstract:
+    def __init__(self, crud_api: CRUDBase):
+        self._crud = crud_api
+
+    async def get_one(self, *args, **kwargs):
+        pass
+
+    async def get_all(
+            self,
+            *args, **kwargs
+    ):
+        pass
+
+    async def put_one(
+            self,
+            *args, **kwargs
+    ):
+        pass
+
+    async def post_one(
+            self,
+            *args, **kwargs
+    ):
+        pass
+
+    async def delete_one(self, *args, **kwargs):
+        pass
+
+
 def add_crud_route_factory(
         router: APIRouter,
         crud: CRUDBase,
@@ -12,6 +41,7 @@ def add_crud_route_factory(
         update_schema: BaseModel,
         response_model: typing.Optional[typing.Type[typing.Any]] = None,
         path_suffix: str = '/',
+        router_method_class: typing.Optional[RouterAbstract] = None,
         # id_field_name: str = 'item_id',
         get_one_route: bool = True,
         get_all_route: bool = True,
@@ -31,18 +61,16 @@ def add_crud_route_factory(
     schema 也可以客製
     """
 
-    class RouterMethod:
-        def __init__(self):
-            self.crud = crud
+    class RouterMethod(RouterAbstract):
 
         async def get_one(self, item_id: typing.Any):
-            return self.crud.get(item_id)
+            return self._crud.get(item_id)
 
         async def get_all(
                 self,
                 pagination: Pagination = Depends()
         ):
-            query = self.crud.query()
+            query = self._crud.query()
             return await pagination.paginate(query)
 
         async def put_one(
@@ -50,22 +78,23 @@ def add_crud_route_factory(
                 item_id: typing.Any,
                 _update_schema: update_schema
         ):
-            return self.crud.update_by_id(item_id, _update_schema)
+            return self._crud.update_by_id(item_id, _update_schema)
 
         async def post_one(
                 self,
                 _create_schema: create_schema
         ):
-            return self.crud.create(_create_schema)
+            return self._crud.create(_create_schema)
 
         async def delete_one(self, item_id: typing.Any):
-            return self.crud.remove_by_id(item_id)
+            return self._crud.remove_by_id(item_id)
 
-    router_method = RouterMethod()
+    _RouterMethodClass = router_method_class or RouterMethod
+    router_method_instance = _RouterMethodClass(crud)
     if get_all_route:
         router.add_api_route(
             f'{path_suffix}',
-            router_method.get_all,
+            router_method_instance.get_all,
             name='Get All',
             response_model=get_pagination_schema(response_model),
             methods=['GET']
@@ -73,7 +102,7 @@ def add_crud_route_factory(
     if get_one_route:
         router.add_api_route(
             f'/{{item_id}}{path_suffix}',
-            router_method.get_one,
+            router_method_instance.get_one,
             name='Get One',
             response_model=response_model,
             methods=['GET']
@@ -81,7 +110,7 @@ def add_crud_route_factory(
     if post_one_route:
         router.add_api_route(
             f'{path_suffix}',
-            router_method.post_one,
+            router_method_instance.post_one,
             name='Post One',
             response_model=response_model,
             methods=['POST']
@@ -89,7 +118,7 @@ def add_crud_route_factory(
     if put_one_route:
         router.add_api_route(
             f'/{{item_id}}{path_suffix}',
-            router_method.put_one,
+            router_method_instance.put_one,
             name='Put One',
             response_model=response_model,
             methods=['PUT']
@@ -97,7 +126,7 @@ def add_crud_route_factory(
     if delete_one_route:
         router.add_api_route(
             f'/{{item_id}}{path_suffix}',
-            router_method.delete_one,
+            router_method_instance.delete_one,
             name='Delete One',
             methods=['DELETE'],
             status_code=204
